0c99f5a98e6b733b140447ba41bf66b52cfc38e1
Mariano Troncoso <petotronco@gmail.com>
2020-04-19 15:37:12 -0300

    Primera insercion en la base de datos

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,14 +1,29 @@
 // dentro del controlador tenemos funciones con las cuales
 // nos asociamos al modelo y al routing
 
+// importamos el modelo
+const Paciente = require('../models/Paciente')
+
 // cuando se crea un nuevo cliente
-exports.nuevoCliente = (req, res, next) => {
-    // to-do: insertar en la base de datos 
+exports.nuevoCliente = async (req, res, next) => {
+    // insertar en la base de datos 
     // req.body es lo que el usuario envia, lo trabajamos con body-parse
-    console.log(req.body)
+
+    // crear objeto de pacinte con datos de req.body
+    const paciente = new Paciente(req.body);
+
+    try {
+        // guardar en la base de datos
+        await paciente.save();
+        // enviar respuesta hacia la API
+        // lo que esta en las {} es lo que pasamos a la funcion
+        res.json({mensaje: 'El cliente se agregó correctamente'});
+        
+    } catch (error) {
+        console.log(error);
+        next();
+    }
 
 
-    // enviar respuesta hacia la API
-    // lo que esta en las {} es lo que pasamos a la funcion
-    res.json({mensaje: 'El cliente se agregó correctamente'});
+    
 }
