d2b833fa063c4e95850f4f3c8ed84aa61fd3ac3d
Mariano Troncoso <petotronco@gmail.com>
2020-04-19 14:42:32 -0300

    conexion con mongo

--- API/index.js
+++ API/index.js
@@ -1,8 +1,21 @@
 // archivo principal de configuracion del servidor
 const express = require('express');
 
+// mongo tiene un ORM llamado mongoose, el cual 
+// nos permite interactuar en la base de datos de una forma mas sencilla
+// importamos mongoose para tener las conexiones
+const mongoose = require('mongoose');
+
 // crear el servidor, lo nombramos "app"
 const app = express();
+
+// conectar nuestra aplicacion con mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
 
 // express no tiene nada, vos tenes que ir agregando lo que necesites
 
